5c79ed5b017c226f57c1cb3e8bdc58df53948056
Amy Ma <amyma402@gmail.com>
2021-09-15 18:49:44 -0700

    Add comments for functions in .c files

--- LoadBalancer.c
+++ LoadBalancer.c
@@ -4,3 +4,31 @@
  * and open the template in the editor.
  */
 
+#include "LoadBalancer.h"
+
+
+/*
+ * Initializes the load balancer
+ * @param int batch size
+ */
+balancer* balancer_create(int batch_size) {
+ 
+    
+}
+
+/* 
+ * Shuts down the load balancer and ensures outstanding batches are completed
+ * @param double pointer to load balancer
+ */
+void balancer_destroy(balancer** lb){
+    
+    
+}
+
+/*
+ * Adds a job to the load balancer (job must be protected by mutex)
+ */
+void balancer_add_job(balancer **lb, int user_id, int data, int* data_return){
+    
+    
+}
